bfde7da8a98baa6c3a824e2bf64608297d49cb26
v-v-vlad <vladiystr@gmail.com>
2024-11-27 23:23:22 +0300

    Add files via upload

---
+++ js/typed.init.js
@@ -0,0 +1,9 @@
+// Typed Text
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 140,
+    loop: true,
+    strings: [
+        'QA engineer', 'Инженер оценки качества ПО',
+    ],
+});
